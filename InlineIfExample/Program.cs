659879ccfea2bc5d20209e6a93f80dd5fcbedf1f
Adriana Disano <adrianadisano@gmail.com>
2018-07-24 06:16:23 -0500

    Add project files.

---
+++ InlineIfExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InlineIfExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
